72f8571b4f1412580b0c287705f3d2ddf2a0611f
awfixer <git@awfixer.me>
2025-09-17 16:04:02 -0700

    update

---
+++ robolectric-shadows/shadows/framework/src/main/java/org/robolectric/shadows/ShadowWindowManagerGlobal.java
@@ -0,0 +1,41 @@
+package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
+import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
+
+import android.content.Context;
+import android.os.Looper;
+import android.os.ServiceManager;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
+
+@Implements(value = WindowManagerGlobal.class, isInAndroidSdk = false,
+    minSdk = JELLY_BEAN_MR1, looseSignatures = true)
+public class ShadowWindowManagerGlobal {
+
+  @Resetter
+  public static void reset() {
+    ReflectionHelpers.setStaticField(
+        WindowManagerGlobal.class, "sDefaultWindowManager", null);
+  }
+
+  @Implementation(minSdk = JELLY_BEAN_MR2)
+  public static Object getWindowSession() {
+    return null;
+  }
+
+  @Implementation(maxSdk = JELLY_BEAN_MR1)
+  public static Object getWindowSession(Looper looper) {
+    return null;
+  }
+
+  @Implementation
+  public static Object getWindowManagerService() {
+    return IWindowManager.Stub.asInterface(ServiceManager.getService(Context.WINDOW_SERVICE));
+  }
+
+}
